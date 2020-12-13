638ecb749ed392adbbc130c36ae8fde6123d9658
Malik Albeik <malikalb@petco.com>
2020-12-13 19:37:53 +0300

    ci: added production/develpment CI/CD

--- fe/src/index.js
+++ fe/src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { AppContainer } from "react-hot-loader";
+// import { AppContainer } from "react-hot-loader";
 
 import Root from "./Root";
 
 ReactDOM.render(
     // <AppContainer>
-        <Root />
+    <Root />
     // </AppContainer>
     ,
     document.querySelector("#root")
