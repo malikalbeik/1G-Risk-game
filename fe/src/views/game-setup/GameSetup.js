ea0c3c743224999deec50529434e8ddabf1aad63
Yahya Mahmoud Ahmed Elnouby Mohamed <yelnouby26@yahoo.com>
2020-12-20 15:45:25 +0300

    Update: Color Picker colors changed to represent same colors of the board game troops

--- fe/src/views/game-setup/GameSetup.js
+++ fe/src/views/game-setup/GameSetup.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import { v4 as uuidv4 } from "uuid";
 import { TwitterPicker } from 'react-color';
-import { BREAKPOINTS } from "../../config/gameConstants";
+import { BREAKPOINTS, COLORS } from "../../config/gameConstants";
 import backgroundImage from "../../assets/background.jpg";
 import {
     Container, Button, Row, Col, FormGroup, Input, Label
@@ -97,9 +97,11 @@ class GameSetup extends Component {
                                                     </ColorContainer>
                                                     {this.state.displayColorPickers[index] ? <ColorPickerPopover>
                                                         <ColorPickerCover onClick={() => this.handleColorPickerClose(index)} />
+                                                        
                                                         <TwitterPicker
                                                             color="#36c"
                                                             triangle="top-left"
+                                                            colors= {COLORS}
                                                             onChange={color => {
                                                                 var players = this.state.players;
                                                                 players[index].color = color.hex;
